de3fda8bbf0fc8f2815d748ccc0a325150428dca
Lulwah Almisfer <lulukalmisfer@hotmail.com>
2025-01-09 13:39:51 +0300

    Events: create a daily job to update the db with new changes + exposse an api with all events types

---
+++ src/main/java/org/example/tasi_dividens/repositories/DividendEventRepository.java
@@ -0,0 +1,10 @@
+package org.example.tasi_dividens.repositories;
+
+import org.example.tasi_dividens.models.DividendEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface DividendEventRepository extends JpaRepository<DividendEvent, Long> {
+    boolean existsBySymbolAndTypeAndEventDate(String symbol, String type, LocalDate eventDate);
+}
