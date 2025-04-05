629c3d60e652c198097aa569df775f9fd55239ea
Lulwah Almisfer <con-lalmasfer@pif.gov.sa>
2025-04-05 13:44:45 +0300

    API: add get all Dividends endpoint

---
+++ src/main/java/org/example/tasi_dividens/TasiDividensApplication.java
@@ -0,0 +1,14 @@
+package org.example.tasi_dividens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TasiDividensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasiDividensApplication.class, args);
+	}
+
+}
